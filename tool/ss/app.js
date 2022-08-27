7319464b33bf7effcd1ce867936db6f88c8600ab
Big-oh-learn <shubhamingale779@gmail.com>
2022-08-27 15:30:09 +0530

    tool upload
    adding actual core tool to repo

---
+++ tool/ss/app.js
@@ -0,0 +1,38 @@
+var input,output;
+render=(val, type)=>{
+    let htmlBlock="",htmlLine,tempBlock;
+    if(type=="ip"){
+        tempBlock=hljs.highlight(val,{language:lang||"c"}).value
+        size=(tempBlock.split("\n").length+"").length;
+        for(i in tempBlock.split("\n")){
+            str = padLeadingZeros(parseInt(i)+1,size);
+            htmlLine=str+". "+tempBlock.split("\n")[i]+"<br>";
+            htmlBlock+=htmlLine;
+        }
+        $("p.input").innerHTML=htmlBlock;
+    }else{
+        $("span.output").innerText=val;
+    }
+}
+copyToClipboard=(but)=>{
+    let range = new Range();
+    range.setStart($("div.outputBlock"), 0);
+    range.setEnd($("div.outputBlock"),10);
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(range);
+    document.execCommand('copy');
+    document.getSelection().removeAllRanges();
+    but.innerText="copied";
+    setTimeout(e=>{
+        but.innerText="Copy"
+    },1000)
+}
+function padLeadingZeros(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+function PRINT(){
+    sessionStorage.setItem("printHtml",$(".outputBlock").innerHTML)
+    window.location.assign("print.html");
+}
