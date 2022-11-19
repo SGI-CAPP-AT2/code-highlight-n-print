d7eb49afe7ce5c2546d39f2c09801a18eb722366
Big-oh-learn <shubhamingale779@gmail.com>
2022-11-19 12:30:40 +0530

    Themes support added

---
+++ tool/themes/handle.js
@@ -0,0 +1,24 @@
+var inputTarget = $("#themeList");
+fetch("themes/themes.json").then(d=>d.json()).then(json=>{
+    inputTarget.innerHTML="";
+    for(let theme of json){
+        inputTarget.innerHTML+=`
+        <option value="${theme.className}">${theme.name}</option>
+        `;
+    }
+    inputTarget.disabled=false;
+});
+inputTarget.addEventListener("change",ev=>{
+    setTheme(ev.target.value)
+});
+if(localStorage.themeCode){
+    document.body.classList.add(localStorage.themeCode)
+}else{
+    localStorage.themeCode="def";
+    document.body.classList.add(localStorage.themeCode)
+}
+var setTheme = (theme) =>{
+    document.body.classList.remove(localStorage.themeCode)
+    localStorage.themeCode=theme;
+    document.body.classList.add(localStorage.themeCode)
+}
