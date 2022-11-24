53c4490a703f1df8cffbf00568c3a148962c5e70
Big-oh-learn <shubhamingale779@gmail.com>
2022-11-24 18:11:22 +0530

    🛠️ theme set fix - set theme was not selected in theme input

--- tool/themes/handle.js
+++ tool/themes/handle.js
@@ -1,9 +1,10 @@
 var inputTarget = $("#themeList");
 fetch("themes/themes.json").then(d=>d.json()).then(json=>{
     inputTarget.innerHTML="";
+    var ifDefault="";
     for(let theme of json){
         inputTarget.innerHTML+=`
-        <option value="${theme.className}">${theme.name}</option>
+        <option ${(theme.className==localStorage.themeCode)?"selected":""} value="${theme.className}">${theme.name}</option>
         `;
     }
     inputTarget.disabled=false;
